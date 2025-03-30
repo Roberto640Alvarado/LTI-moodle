0667e994e36b0e3dac72fe6be4b3cde122f3b550
Roberto640Alvarado <00176620@uca.edu.sv>
2025-03-30 10:38:01 -0600

    :sparkles: Agregando conf LTI

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const lti = require('ltijs').Provider
+
+//Inicialización del servidor LTI
+lti.setup(
+  'LTIKEY123', 
+  {
+    url: process.env.MONGO_URL
+  },
+  {
+    appUrl: '/', 
+    loginUrl: '/login',
+    cookies: {
+      secure: true,
+      sameSite: 'None'
+    }
+  }
+)
+
+//Manejador de conexión LTI
+lti.onConnect(async (token, req, res) => {
+  const idToken = res.locals.token
+  const nombre = idToken.userInfo.name
+  const rol = idToken.userInfo.roles?.[0] || 'Desconocido'
+  const curso = idToken.platformContext.title
+  const tarea = idToken.resourceLink.title
+
+  return res.send(`
+    <h1>Hola, ${nombre}!</h1>
+    <p>Tu rol es: ${rol}</p>
+    <p>Estás en el curso: ${curso}</p>
+    <p>Entraste desde la tarea: ${tarea}</p>
+  `)
+})
+
+// Inicia el servidor LTI
+lti.deploy({ serverless: true }).then(() => {
+  console.log('Servidor LTI activo en Render.')
+})
